70d41a83eb830d96adacd5f325eed93c722b8338
Yahiasherif002 <Yahiasheriif87@gmail.com>
2024-12-15 23:04:45 +0200

    Add project files.

---
+++ AlgorithmProject/Heap-Sort Algorithm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProject
+{
+    internal class Heap_Sort_Algorithm
+    {
+        public static void HeapSortAlgorithm(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+                MaxHeapify(arr, n, i);
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                Swap(arr, 0, i);
+                MaxHeapify(arr, i, 0);
+            }
+        }
+
+        private static void MaxHeapify(int[] arr, int heapSize, int rootIndex)
+        {
+            int largest = rootIndex;
+            int leftChild = 2 * rootIndex + 1;
+            int rightChild = 2 * rootIndex + 2;
+
+            if (leftChild < heapSize && arr[leftChild] > arr[largest])
+                largest = leftChild;
+
+            if (rightChild < heapSize && arr[rightChild] > arr[largest])
+                largest = rightChild;
+
+            if (largest != rootIndex)
+            {
+                Swap(arr, rootIndex, largest);
+                MaxHeapify(arr, heapSize, largest);
+            }
+        }
+
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            HeapSortAlgorithm(arr);
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
---
+++ AlgorithmProject/KruskalAlgorithm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProject
+{
+    internal class KruskalAlgorithm
+    {
+        public class Edge : IComparable<Edge>
+        {
+            public int Source, Destination, Weight;
+
+            public int CompareTo(Edge other)
+            {
+                return this.Weight.CompareTo(other.Weight);
+            }
+        }
+
+        public class Graph
+        {
+            public int Vertices, Edges;
+            public Edge[] EdgeArray;
+
+            public Graph(int vertices, int edges)
+            {
+                Vertices = vertices;
+                Edges = edges;
+                EdgeArray = new Edge[edges];
+                for (int i = 0; i < edges; i++)
+                    EdgeArray[i] = new Edge();
+            }
+
+            public int Find(int[] parent, int i)
+            {
+                if (parent[i] != i)
+                    parent[i] = Find(parent, parent[i]);
+                return parent[i];
+            }
+
+            public void Union(int[] parent, int[] rank, int x, int y)
+            {
+                int rootX = Find(parent, x);
+                int rootY = Find(parent, y);
+
+                if (rank[rootX] < rank[rootY])
+                    parent[rootX] = rootY;
+                else if (rank[rootX] > rank[rootY])
+                    parent[rootY] = rootX;
+                else
+                {
+                    parent[rootY] = rootX;
+                    rank[rootX]++;
+                }
+            }
+
+            public void KruskalMST()
+            {
+                Array.Sort(EdgeArray);
+
+                int[] parent = new int[Vertices];
+                int[] rank = new int[Vertices];
+
+                for (int v = 0; v < Vertices; v++)
+                {
+                    parent[v] = v;
+                    rank[v] = 0;
+                }
+
+                List<Edge> result = new List<Edge>();
+                int edgeCount = 0, i = 0;
+
+                while (edgeCount < Vertices - 1 && i < Edges)
+                {
+                    Edge nextEdge = EdgeArray[i++];
+
+                    int x = Find(parent, nextEdge.Source);
+                    int y = Find(parent, nextEdge.Destination);
+
+                    if (x != y)
+                    {
+                        result.Add(nextEdge);
+                        edgeCount++;
+                        Union(parent, rank, x, y);
+                    }
+                }
+
+                Console.WriteLine("Edges in the Minimum Spanning Tree:");
+                foreach (var edge in result)
+                {
+                    Console.WriteLine($"{edge.Source} -- {edge.Destination} == {edge.Weight}");
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int vertices = 4;
+            int edges = 5;
+
+            Graph graph = new Graph(vertices, edges);
+
+            graph.EdgeArray[0].Source = 0;
+            graph.EdgeArray[0].Destination = 1;
+            graph.EdgeArray[0].Weight = 10;
+
+            graph.EdgeArray[1].Source = 0;
+            graph.EdgeArray[1].Destination = 2;
+            graph.EdgeArray[1].Weight = 6;
+
+            graph.EdgeArray[2].Source = 0;
+            graph.EdgeArray[2].Destination = 3;
+            graph.EdgeArray[2].Weight = 5;
+
+            graph.EdgeArray[3].Source = 1;
+            graph.EdgeArray[3].Destination = 3;
+            graph.EdgeArray[3].Weight = 15;
+
+            graph.EdgeArray[4].Source = 2;
+            graph.EdgeArray[4].Destination = 3;
+            graph.EdgeArray[4].Weight = 4;
+
+            graph.KruskalMST();
+        }
+    }
+}
